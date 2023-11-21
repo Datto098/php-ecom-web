8df49da4baae566b2d805ab552fc218e7d27f654
Datto098 <dat61222@gmail.com>
2023-11-21 11:36:13 +0700

    Update database

--- database/ecom_web_demo.sql
+++ database/ecom_web_demo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: localhost
--- Thời gian đã tạo: Th10 19, 2023 lúc 04:25 AM
+-- Thời gian đã tạo: Th10 21, 2023 lúc 05:35 AM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.1.17
 
@@ -59,7 +59,7 @@ INSERT INTO `categories` (`category_id`, `category_name`, `parent_id`) VALUES
 (1, 'Nam', NULL),
 (2, 'Nữ', NULL),
 (3, 'Trẻ em', NULL),
-(4, 'Siêu sale tháng 11', NULL),
+(4, 'Siêu sale', NULL),
 (14, 'Áo', 1),
 (15, 'Quần nam', 1),
 (16, 'Giày & Dép', 1),
@@ -185,7 +185,11 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`product_id`, `product_img`, `product_desc`, `product_brand`, `product_name`, `product_price`, `category_id`) VALUES
 (17, '26a096a6a1cd164effeb6e7bcbe9e93f.jpg', 'Áo sơ mi croptop trẻ trung, năng động, thích hợp với nàng yêu thích sự cá tính.\r\n\r\nThiết kế áo dáng croptop với cổ đức cùng tay dài. \r\n\r\nÁo được tạo kiểu 2 lớp: 1 lớp cổ V bên trong và  1 lớp áo giả khoác bên ngoài. \r\n\r\nNàng có thể lựa chọn mix áo cùng chân váy, quần dài, quần short đều rất phù hợp. ', 'Ivymoda', 'ÁO SƠ MI CROPTOP', 1290000, 43),
-(18, '3dd38dc22de76b4bb376ce28cfd0fcfa.jpg', 'Sang trọng, thời thượng qua mẫu sơ mi lụa được Quý cô yêu thích từ nhà IVY moda. \r\n\r\nÁo cổ thuyền, dáng thường, thiết kế vai chờm độc đáo cùng điểm nhấn rút vai mới lạ. Để từ đó tạo bên tay lệch bắt mắt. \r\n\r\nCùng với đó, thiết kế lựa chọn chất liệu lụa mềm mại, mang đến cảm giác mặc nhẹ nhàng và vô cùng thoải mái. ', 'Ivymoda', 'ÁO LỤA CỔ THUYỀN', 890000, 43);
+(18, '3dd38dc22de76b4bb376ce28cfd0fcfa.jpg', 'Sang trọng, thời thượng qua mẫu sơ mi lụa được Quý cô yêu thích từ nhà IVY moda. \r\n\r\nÁo cổ thuyền, dáng thường, thiết kế vai chờm độc đáo cùng điểm nhấn rút vai mới lạ. Để từ đó tạo bên tay lệch bắt mắt. \r\n\r\nCùng với đó, thiết kế lựa chọn chất liệu lụa mềm mại, mang đến cảm giác mặc nhẹ nhàng và vô cùng thoải mái. ', 'Ivymoda', 'ÁO LỤA CỔ THUYỀN', 890000, 43),
+(19, '167bfea45ab9765d9161a2bb2e8f18e7.jpg', 'Thiết kế áo kiểu nhẹ nhàng, thanh lịch và tinh tế.\r\n\r\nÁo dáng suông, cổ tròn cách điệu, có khuy cài. Tay áo thiết kế cánh dơi nhẹ, mang  đến cảm giác mềm mại.\r\n\r\nĐặc biệt, áo sử dụng chất liệu lụa sang trọng, thích hợp diện trong nhiều hoàn cảnh khác nhau. ', 'Ivymoda', 'ÁO LỤA TAY KIỂU', 850000, 44),
+(20, '8594ae701bddf9d9c0127a350d9dff28.jpg', 'Nàng công sở yêu thích một chút điệu đà, nữ tính chắc chắn không nên bỏ lỡ thiết kế sơ mi Peplum cách điệu lần này.\r\n\r\nÁo được lên dáng xếp tà tạo cổ chữ V quyến rũ, phần tay chờm có điểm rút dây điều chỉnh độ co. Thân dưới áo tạo tà Peplum độc đáo, nhẹ nhàng, mềm mại nhưng vẫn thật  sang trọng và thời thượng.\r\n\r\nGợi ý nàng mix áo cùng chân váy hoặc quần đều phù hợp, thêm các phụ kiện như túi sách, vòng cổ... để hoàn thiện cho Outfit của  mình. ', 'Ivymoda', 'ÁO SƠ MI XẾP TÀ PEPLUM', 890000, 44),
+(21, '5417f5b64699195d74da5ccda0b979c0.jpeg', 'Set đồ bao gồm áo croptop và zuýp bút chì. Áo dáng crop xếp ly dưới, tay xếp ly phồng. Áo cái bằng hàng khuy trắng thanh lịch. Chân váy cạp cao, cạp may tạo kiểu, độ dài qua đầu gối. Set đồ được làm từ chất liệu tuysi cao cấp giúp giữ form dáng luôn chỉn chu.\r\n\r\nChẳng cần phải đau đầu khi phối đồ, hãy lựa chọn một outfit theo set khi đi làm hoặc xuống phố. Bộ đôi áo croptop và zuýp sẽ giúp tôn lên dáng xinh của nàng.', 'Ivymoda', 'SET ÁO CROPTOP TAY BỒNG VÀ ZUÝP BÚT CHÌ', 445000, 45),
+(22, '071952a1a3767560aef35a53b1d80109.jpg', 'Áo kiểu nữ cổ V tay lửng, cổ tay bo chun phồng, áo cài bằng hàng khuy phía trước. Dáng áo croptop, eo may chun nhúm bản to trẻ trung, tôn vòng eo nàng. Chất liệu vải lụa mềm mịn, thoáng mát cùng họa tiết kẻ ngang và viền lông nổi độc đáo.\r\n\r\nThiết kế giúp làm tôn lên đường nét tinh tế, mềm mại và quyến rũ của phái đẹp. Kết hợp áo với chân vấy ngắn hoặc jeans là nàng đã có 1 outfit đi chơi cực trẻ trung và nữ tính.\r\n\r\n', 'Ivymoda', 'ÁO CROPTOP LỤA HỌA TIẾT KẺ NGANG', 950000, 45);
 
 -- --------------------------------------------------------
 
@@ -207,7 +211,11 @@ CREATE TABLE `product_details` (
 
 INSERT INTO `product_details` (`detail_id`, `product_id`, `product_color`, `product_size`, `product_quantity`) VALUES
 (25, 17, 'Trắng', 'M', 10),
-(26, 18, 'Vàng mustard', 'L', 10);
+(26, 18, 'Vàng mustard', 'L', 10),
+(27, 19, 'Vàng mustard', 'L', 10),
+(28, 20, 'Xanh ghi đá', 'L', 10),
+(29, 21, 'Hồng san hô', 'L', 10),
+(30, 22, 'Họa tiết Xanh tím than', 'L', 10);
 
 -- --------------------------------------------------------
 
@@ -239,7 +247,31 @@ INSERT INTO `product_images` (`image_id`, `href_value`, `product_id`) VALUES
 (51, 'a6528be04d16a3ee954d2ddeaf03d744.jpg', 26),
 (52, '99e07a9b3279837295b83734591233ac.jpg', 26),
 (53, 'af19db1a796fce1cfa55ec52d0a139e0.jpg', 26),
-(54, '51f813755233b002d8f4464b12771e32.jpg', 26);
+(54, '51f813755233b002d8f4464b12771e32.jpg', 26),
+(55, '167bfea45ab9765d9161a2bb2e8f18e7.jpg', 27),
+(56, '470a22e2e33e05c4e2b45a52dd47eaab.jpg', 27),
+(57, '742c30390328ec2b387a338c3f0b4643.jpg', 27),
+(58, 'afb8c189e6a098cdc0637139b6ff4984.jpg', 27),
+(59, 'f16bdd5c840f109f4106c0618599f786.jpg', 27),
+(60, 'fc84b7fee0da714d477993096facc31c.jpg', 27),
+(61, '2d2cbacf48a205d45e4b608d906ff484.jpg', 28),
+(62, '10dcdc6dd20bd2f361a187cf2386ca2b.jpg', 28),
+(63, '8594ae701bddf9d9c0127a350d9dff28.jpg', 28),
+(64, 'a7b2fe4e401425b92d044ef2487a2ab2.jpg', 28),
+(65, 'e9dfd5cc02895cdaaaea1e50135777f7.jpg', 28),
+(66, 'eab97f8303b60cf92d8449dfa75d3133.jpg', 28),
+(67, '1d84c3d47bcb9e2685a46572474ed740.jpeg', 29),
+(68, '5417f5b64699195d74da5ccda0b979c0.jpeg', 29),
+(69, '9f5ddb834d62aeed037385b8ada95974.jpeg', 29),
+(70, '13f6d59d3138952dd85af9cc8b988864.jpeg', 29),
+(71, 'd5635f6a22ae082cabb76f57f70d157d.jpeg', 29),
+(72, '38bd47f3ca762609c59cb8d868d0831b.jpeg', 29),
+(73, '071952a1a3767560aef35a53b1d80109.jpg', 30),
+(74, '568656c0f6e9798a61aec6c21e8a823d.jpg', 30),
+(75, '4b2a4ec1a6f38dd1dc6d48c21ee6ddc4.jpg', 30),
+(76, '9c353e1e0cbd37a15464212174cdb53e.jpg', 30),
+(77, '65ef2a2b239c7ad3783244c216eea5e7.jpg', 30),
+(78, '74f19c391e52814a77f01c1d420eba3c.jpg', 30);
 
 -- --------------------------------------------------------
 
@@ -252,6 +284,18 @@ CREATE TABLE `rates` (
   `rate_value` int(11) NOT NULL,
   `product_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `rates`
+--
+
+INSERT INTO `rates` (`rate_id`, `rate_value`, `product_id`) VALUES
+(1, 5, 17),
+(2, 4, 17),
+(3, 4, 17),
+(4, 4, 17),
+(5, 4, 17),
+(6, 5, 17);
 
 -- --------------------------------------------------------
 
@@ -399,25 +443,25 @@ ALTER TABLE `order_details`
 -- AUTO_INCREMENT cho bảng `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT cho bảng `product_details`
 --
 ALTER TABLE `product_details`
-  MODIFY `detail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `detail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT cho bảng `product_images`
 --
 ALTER TABLE `product_images`
-  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
 
 --
 -- AUTO_INCREMENT cho bảng `rates`
 --
 ALTER TABLE `rates`
-  MODIFY `rate_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `rate_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT cho bảng `shipping_addresses`
